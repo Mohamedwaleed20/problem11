5133c3dc0d6b0d163f5b06880ba057a84107e721
Mohamed waleed <Mohamed waleed@DESKTOP-0AUSUD8>
2024-07-14 14:39:25 +0300

    Add project files.

---
+++ problem11/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.DataAnnotations;
+Console.WriteLine("enter the arr : ");
+string arr = Console.ReadLine();
+int[] numbers = arr.Split(',').Select(int.Parse).ToArray();
+int[] res = new int[numbers.Length];
+Console.Write("[ ");
+for (int i = 0; i < numbers.Length; i++)
+{
+    int product = 1;
+    for (int j = 0; j < numbers.Length; j++)
+    {
+        if (i != j)
+        {
+            product *= numbers[j];
+        }
+    }
+    res[i] = product;
+    Console.Write(res[i]);
+    Console.Write(", ");
+}
+Console.Write(" ]");
+Console.ReadLine();
